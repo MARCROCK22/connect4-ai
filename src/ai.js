358cdfb582f8e69ae301d726e934181206821a6e
MARCROCK22 <marcoscolonrosario@gmail.com>
2021-07-15 01:20:16 -0400

    fix

--- src/ai.js
+++ src/ai.js
@@ -2,7 +2,7 @@ const AI = require("./uwu.js").Connect4AI;
 const Connect4 = require("./normal.js");
 class Connect4AI extends Connect4 {
 	playAI(difficulty = "medium") {
-		const board = new AI();
+		const board = new AI(this.columns, this.lengthArr);
 		for (let i of this.plays) {
 			board.play(i);
 		}
